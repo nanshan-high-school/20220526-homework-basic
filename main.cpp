87a3bd467a30773c3102a58fe4eb12b54b572c37
anyon6 <92091780+anyon6@users.noreply.github.com>
2022-06-09 21:04:51 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,40 +2,43 @@
 #include <vector>
 using namespace std;
 int main(){
-  vector <string> words;
   string a;
-  int c=1;
   int e,d,g;
-  cin>>e;
-  for(int j=0;j<e;j++){
-    cin>>a;
-    if(j==0){
-      cout<<"New! 1\n";
-      words.push_back(a);
-    }else{
-      for(int i=0;i<=words.size();i++){
-        if(a==words[i]){
-          g=0;
-          d=i+1;
-          break;
-        }else{
-          g=1;
-        }
-      }
-      if(g==0){
-        cout<<"Old! ";
-        cout<<d;
-        cout<<"\n";
-        d++;
-      }else{
+  while(cin>>e){
+    vector <string> words;
+    int c=1;
+    for(int j=0;j<e;j++){
+      cin>>a;
+      if(j==0){
+        cout<<"New! 1\n";
         words.push_back(a);
-        cout<<"New! ";
-        cout<<c+1;
-        cout<<"\n";
-        c++;
+      }else{
+        for(int i=0;i<=words.size();i++){
+          if(a==words[i]){
+            g=0;
+            d=i+1;
+            break;
+          }else{
+            g=1;
+          }
+        }
+        if(g==0){
+          cout<<"Old! ";
+          cout<<d;
+          cout<<"\n";
+          d++;
+        }else{
+          words.push_back(a);
+          cout<<"New! ";
+          cout<<c+1;
+          cout<<"\n";
+          c++;
+        }
       }
     }
   }
 }
+
+
 
 
