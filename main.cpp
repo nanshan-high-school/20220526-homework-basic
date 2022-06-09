fa19dac5d7b3febe0b02cd44684245284880593d
anyon6 <92091780+anyon6@users.noreply.github.com>
2022-06-09 20:49:01 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,32 +5,35 @@ int main(){
   vector <string> words;
   string a;
   int c=1;
-  int d=0;
-  int g;
-  cin>>a;
-  words.push_back(a);
-  cout<<"new1\n";
-  while(true){
+  int e,d,g;
+  cin>>e;
+  for(int j=0;j<=e;j++){
     cin>>a;
-    for(int i=0;i<=words.size();i++){
-      if(a==words[i]){
-        g=0;
-        break;
-      }else{
-        g=1;
-      }
-    }
-    if(g==0){
-      cout<<"old";
-      cout<<d+1;
-      cout<<"\n";
-      d++;
-    }else{
+    if(j==0){
+      cout<<"New! 1\n";
       words.push_back(a);
-      cout<<"new";
-      cout<<c+1;
-      cout<<"\n";
-      c++;
+    }else{
+      for(int i=0;i<=words.size();i++){
+        if(a==words[i]){
+          g=0;
+          d=i+1;
+          break;
+        }else{
+          g=1;
+        }
+      }
+      if(g==0){
+        cout<<"Old";
+        cout<<d;
+        cout<<"\n";
+        d++;
+      }else{
+        words.push_back(a);
+        cout<<"New! ";
+        cout<<c+1;
+        cout<<"\n";
+        c++;
+      }
     }
   }
 }
